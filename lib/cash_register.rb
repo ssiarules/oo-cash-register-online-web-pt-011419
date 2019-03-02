b1db62a11d9e35e1864eadf80531f032a27d76dd
Johnson Joseph <jhonsonj88@gmail.com>
2019-03-02 06:34:33 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,15 @@
 
+class CashRegister
+
+  attr_accessor :discount, :total
+
+  def initialize(total = 0)
+    @total = total
+    @discount = 20
+  end
+
+  def add_item(title, price)
+
+  end
+
+end
